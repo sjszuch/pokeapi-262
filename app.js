c0f0ad699b4954cbce19324259ca7e1da535c6ed
Sebastian Szuch <98364552+sjszuch@users.noreply.github.com>
2024-02-29 17:02:52 -0500

    Added additional information to response page

--- app.js
+++ app.js
@@ -19,7 +19,14 @@ app.get('/pokemon', (req, res) => {
     // Retrieves the image
     request('https://pokeapi.co/api/v2/pokemon/' + pokemonName, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            res.send(JSON.parse(body).sprites.front_default);
+
+            res.send('<img src="' + JSON.parse(body).sprites.front_default + '">'
+            + '<h1>' + pokemonName + '</h1>'
+            + '<p>Type: ' + JSON.parse(body).types[0].type.name + '</p>'
+            + '<p>Height: ' + JSON.parse(body).height + '</p>'
+            + '<p>Weight: ' + JSON.parse(body).weight + '</p>'
+            );
+            
         }
         else {
             res.send("Error: " + response.statusCode);
